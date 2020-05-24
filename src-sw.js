3460aee856b1c9ed94faf207677c174332f664df
Hawaijar.js <hawaijar.js@gmail.com>
2020-05-24 23:09:59 +0530

    Added 'Offline Access' and third-party API access capability to Service Worker

--- src-sw.js
+++ src-sw.js
@@ -1,5 +1,19 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
+import {registerRoute} from 'workbox-routing';
+import {NetworkFirst, StaleWhileRevalidate, CacheFirst} from 'workbox-strategies';
+import {CacheableResponsePlugin} from 'workbox-cacheable-response';
 
 console.log('this is my custom service worker');
+
+registerRoute(
+    'http://localhost:3000/users',
+    new CacheFirst({
+      plugins: [
+        new CacheableResponsePlugin({
+          statuses: [0, 200]
+        })
+      ]
+    }),
+  );
 
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);

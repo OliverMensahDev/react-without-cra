b6308d080f2b99cac7be8b19c472e1dededa3f92
Hawaijar.js <hawaijar.js@gmail.com>
2020-05-24 21:58:58 +0530

    Added Service Worker (Vanila)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js').then(registration => {
+            console.log('SW registered: ', registration);
+        }).catch(registrationError => {
+            console.log('SW registration failed: ', registrationError);
+        });
+    });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,22 @@
 const path = require('path');
+const webpack = require("webpack");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+const ReplacePlugin = require('webpack-plugin-replace');
 
 module.exports = {
-    entry:'./src/index.js',
+    entry: './src/index.js',
     output: {
         path: path.join(__dirname, '/dist'),
         filename: 'bundle.js'
     },
-    module:{
-        rules:[
+    optimization: {
+        minimize: false
+    },
+    module: {
+        rules: [
             {
-                test:/\.js$/,
+                test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader'
@@ -21,6 +27,18 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html'
-        })
+        }),
+        new WorkboxPlugin.GenerateSW({
+            // these options encourage the ServiceWorkers to get in there fast
+            // and not allow any straggling "old" SWs to hang around
+            cacheId: 'hawaijar',
+            clientsClaim: true,
+            skipWaiting: true,
+            swDest: 'sw.js'
+        }),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production'),
+            'process.env.PUBLIC_URL': JSON.stringify('../../dist')
+          })
     ]
 }
